78dcc6874a11e585d7901353c84fd1374f5942f3
Pratap Patra <pratappatra078@gmail.com>
2025-11-23 09:39:13 +0530

    stack
    insert at bottom,index question solve

---
+++ 16_stack/Insert_in.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insert_at_bottom(stack<int>&st,int val){
+    stack<int>temp;
+    while(not st.empty()){
+        int curr = st.top();
+        st.pop();
+        temp.push(curr);
+    }
+    //now st is empty and top s in -1
+    st.push(val);
+    while(not temp.empty()){
+        int curr = temp.top();
+        temp.pop();
+        st.push(curr);
+    }
+}
+
+void insert_at_bottom_recursive(stack<int>&st,stack<int>&result,int  val){
+    if(st.empty()){
+        result.push(val);
+        return;
+    }
+    int curr = st.top();
+    st.pop();
+    insert_at_bottom_recursive(st,result,val);
+    result.push(curr);
+}
+
+void insert_at_idx(stack<int>&st,int val,int idx){
+    stack<int> temp;
+    int cnt = 0;
+    while(cnt != st.size()-idx +1){
+        int curr = st.top();
+        st.pop();
+        temp.push(curr);
+    }
+    st.push(val);
+    while(not temp.empty()){
+        int curr = temp.top();
+        st.push(curr);
+        temp.pop();
+    }
+}
+int main() {
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    insert_at_idx(st,15,2);
+
+    while(not st.empty()){
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+    return 0;
+}
